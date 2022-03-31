4c3c12f6c57d1e341c7d34c2515d4af05219d3e6
anandsravi <102493767+anandsravi@users.noreply.github.com>
2022-03-31 15:37:29 -0400

    Update Big.java

--- myprograms/Big.java
+++ myprograms/Big.java
@@ -4,7 +4,8 @@ public class Big {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-               Sysytem.out.println("****8");
+               Sysytem.out.println("****8"); 
+		Sysytem.out.println("$$$$$");		
 	}
 
 }
